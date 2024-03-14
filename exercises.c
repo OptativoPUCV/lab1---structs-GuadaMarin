342fd164113cb51b390157c4d99d16dad6ff4eb3
Your Name <you@example.com>
2024-03-14 20:45:59 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -39,7 +39,7 @@ y su tamaño, y luego devuelva un nuevo arreglo que contenga solo
 los números pares del arreglo original.
 */
 int *filterEvenNumbers(int arr[], int size, int *newSize) {
-  int *newArr=NUll;
+  int *newArr=NULL;
   for(int i=0; i<size; i++){
     if(arr[i]%2==0){
       *newSize= *newSize+1;
